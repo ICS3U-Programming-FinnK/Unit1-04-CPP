6c8017c37ac481c4ebf992e605e003cc818a34a3
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-09-18 23:40:44 +0000

    added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,14 @@
+// Created by: Finn Kitor
+// Created on: September 18th, 2023
+// This program does math calculations
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "8 + 2 = " << 8 + 2 << std::endl;
+    std::cout << "7 - 1 = " << 7 - 1 << std::endl;
+    std::cout << "6 / 2 = " << 6 / 2 << std::endl;
+    std::cout << "8 - 2 / 2 = " << 8 - 2 / 2 << std::endl;
+    std::cout << "283 = " << pow(2, 8) << std::endl;
+    std::cout << "1 / 5 = " << float(1) / float(5) << std::endl;
+}
