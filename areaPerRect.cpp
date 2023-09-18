787a00a9f2527d826c650acdb25404f87679e545
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-09-18 23:49:33 +0000

    added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,13 @@
+// Created by: Finn Kitor
+// Created on: September 18th, 2023
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 3cm X 3cm.
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 3cm and width 3cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 3 * 3 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (3 + 3) << " cm\n";
+}
